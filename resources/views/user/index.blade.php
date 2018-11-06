37d7be3819a8a97e3c34621147d181ca908b2dcd
Grant Gubatan <grantgubatandev@gmail.com>
2018-11-06 18:51:46 +0800

    removing iframe

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -10,8 +10,8 @@
           <div class="card shadow-sm p-3 mb-5 bg-white rounded col-12">
             <div class="card-body">
               <h1>LIVE STOCK MARKET GRAPH</h1>
-              <iframe src="https://s.tradingview.com/widgetembed/?frameElementId=tradingview_d6831&symbol=AAPL%3A&interval=D&symboledit=1&saveimage=1&toolbarbg=f1f3f6&details=1&news=1&studies=%5B%5D&newsvendors=stocktwits%1Fheadlines&hideideas=1&theme=Light&style=1&timezone=Etc%2FUTC&studies_overrides=%7B%7D&overrides=%7B%7D&enabled_features=%5B%5D&disabled_features=%5B%5D&locale=en&utm_medium=widget&utm_campaign=chart&utm_term=AMZN%3A"
-              width="100%" height="600px" frameBorder="0"></iframe>
+              <!-- <iframe src="https://s.tradingview.com/widgetembed/?frameElementId=tradingview_d6831&symbol=AAPL%3A&interval=D&symboledit=1&saveimage=1&toolbarbg=f1f3f6&details=1&news=1&studies=%5B%5D&newsvendors=stocktwits%1Fheadlines&hideideas=1&theme=Light&style=1&timezone=Etc%2FUTC&studies_overrides=%7B%7D&overrides=%7B%7D&enabled_features=%5B%5D&disabled_features=%5B%5D&locale=en&utm_medium=widget&utm_campaign=chart&utm_term=AMZN%3A"
+              width="100%" height="600px" frameBorder="0"></iframe> -->
             </div>
           </div>
         </div>
