075684931258ca4489bd39b3ffc123c9e00becb4
Grant Gubatan <grantgubatandev@gmail.com>
2018-12-17 21:01:40 +0800

    test changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    CLIENT PORTAL
+                    CLIENT PORTAL x
                 </div>
 
                 <!-- <div class="links">
